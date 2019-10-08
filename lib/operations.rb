efabdf30bbd9dea32d5240ae2961bf9c4f8e66d6
Mei Wong <fongmeiw@gmail.com>
2019-10-08 15:59:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed < 40 && speed > 60
-  true
+    true
   elsif speed >= 50
-  false
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 && speed > 60 ? true : false
 end
 	
 
